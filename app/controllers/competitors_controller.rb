cdaf6c16ae794e2a54a0e16a486c5c6d63863776
Zoli <virag.zoltan@ombrello.hu>
2012-07-13 14:33:30 +0200

    Facebook development 09

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -1,21 +1,28 @@
 class CompetitorsController < ApplicationController
   
-  before_filter :parse_facebook_cookies
+  #before_filter :parse_facebook_cookies
+  before_filter :parse_signed_request
 
-  def parse_facebook_cookies
-    @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+  def parse_signed_request
+    @oauth = Koala::Facebook::OAuth.new
+    @signed_request = @oauth.parse_signed_request(params["signed_request"])
   end
+  
+  
+  #def parse_facebook_cookies
+   # @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+  #end
 
 	def add
-		begin
+#		begin
 		  @competitor = Competitors.new
-		  @access_token = @facebook_cookies["access_token"]
-      @graph = Koala::Facebook::GraphAPI.new(@access_token)
-      @likes = @graph.get_connections("me", "likes")
-      @status = "OK"
-    rescue
-      nil
-    end
+# 	  @access_token = @facebook_cookies["access_token"]
+#     @graph = Koala::Facebook::GraphAPI.new(@access_token)
+#     @likes = @graph.get_connections("me", "likes")
+#     @status = "OK"
+#   rescue
+#     nil
+#   end
 	end
 	
 	def create
